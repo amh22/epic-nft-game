4649e17ad64b806d208ec9178e9766acace8554d
Andrew Henry <andrewmhenry@me.com>
2021-11-03 16:54:58 +0800

    :pencil: feat(Cleanup): cleaned up some unused code

--- solidity/contracts/MyEpicGame.sol
+++ solidity/contracts/MyEpicGame.sol
@@ -172,20 +172,20 @@ contract MyEpicGame is ERC721 {
       }
     }
 
-    /* ------ CALLED FROM THE FRONTEND to GET ALL NFT HOLDER DETAILS ------------- */
-  function getNFTHolder(address sender) public view returns (CharacterAttributes memory) {
-    // Get the tokenId of the user's character NFT
-    uint256 userNftTokenId = nftHolders[sender];
-    // If the user has a tokenId in the map, return their character.
-    if (userNftTokenId > 0) {
-      return nftHolderAttributes[userNftTokenId];
-    }
-    // Else, return an empty character.
-    else {
-      CharacterAttributes memory emptyStruct;
-      return emptyStruct;
-    }
-  }
+  //   /* ------ CALLED FROM THE FRONTEND to GET ALL NFT HOLDER DETAILS ------------- */
+  // function getNFTHolder(address sender) public view returns (CharacterAttributes memory) {
+  //   // Get the tokenId of the user's character NFT
+  //   uint256 userNftTokenId = nftHolders[sender];
+  //   // If the user has a tokenId in the map, return their character.
+  //   if (userNftTokenId > 0) {
+  //     return nftHolderAttributes[userNftTokenId];
+  //   }
+  //   // Else, return an empty character.
+  //   else {
+  //     CharacterAttributes memory emptyStruct;
+  //     return emptyStruct;
+  //   }
+  // }
 
   function getAllDefaultCharacters() public view returns (CharacterAttributes[] memory) {
     return defaultCharacters;
@@ -198,8 +198,6 @@ contract MyEpicGame is ERC721 {
   function getAllPlayers() public view returns (AllPlayers[] memory) {
     return gameUsers;
   }
-
-
 
 
 
